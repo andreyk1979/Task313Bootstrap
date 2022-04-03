bb6e8902eb170de91d4702edd8bda30fb32d8168
andreyk1979 <andrey.kuimov@gmail.com>
2022-04-03 22:29:52 +0200

    some things

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,29 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.Service.RoleService;
+import com.example.demo.Service.UserService;
+import com.example.demo.models.Role;
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
-
-
+    @GetMapping("/user")
+    public String infoUser(@AuthenticationPrincipal User user, ModelMap model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", user.getRoles());
+        return "user_page";
+    }
 }
