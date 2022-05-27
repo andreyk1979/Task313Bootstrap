bffacdc4b2d7d86c16829d96430e5fbcd3b488e9
andreyk1979 <andrey.kuimov@gmail.com>
2022-05-27 14:43:30 +0200

    remove in configure passwordEncoder(passwordEncoder())

--- src/main/java/com/kuimov/pp/task313/security/SecurityConfig.java
+++ src/main/java/com/kuimov/pp/task313/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
